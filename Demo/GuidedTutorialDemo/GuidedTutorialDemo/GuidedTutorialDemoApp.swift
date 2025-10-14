d05f71ead41672091d4749b126e76577743ef474
Roman <roman.malynovsky@gmail.com>
2025-10-14 14:57:05 +0300

    added demo

---
+++ Demo/GuidedTutorialDemo/GuidedTutorialDemo/GuidedTutorialDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidedTutorialDemoApp.swift
+//  GuidedTutorialDemo
+//
+//  Created by Roman Malynovsky on 14.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuidedTutorialDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Package.swift
+++ Package.swift
@@ -5,20 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "GuidedTutorial",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GuidedTutorial",
             targets: ["GuidedTutorial"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "GuidedTutorial"),
-        .testTarget(
-            name: "GuidedTutorialTests",
-            dependencies: ["GuidedTutorial"]
-        ),
+        
+        
+//            .executableTarget(
+//                name: "TutorialExample",
+//                dependencies: ["GuidedTutorial"],
+//                path: "Demo/GuidedTutorialDemo"
+//            ),
+        
+            .testTarget(
+                name: "GuidedTutorialTests",
+                dependencies: ["GuidedTutorial"]
+            ),
     ]
 )
